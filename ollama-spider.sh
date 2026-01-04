65753e6bb534aefd51e62664c005278416e20c67
Nilzao <288571+nilzao@users.noreply.github.com>
2026-01-04 11:31:24 -0400

    more details to csv file

--- ollama-spider.sh
+++ ollama-spider.sh
@@ -10,13 +10,28 @@ while read -r link; do
 done < models/002_search_links.html
 
 for file in models/*-tags.html; do
+  raw_date=$(grep -oP 'title="\K[A-Z][a-z]{2} [ 0-9]{1,2}, [0-9]{4}' "$file" | head -n1)
+  if [[ -n "$raw_date" ]]; then
+    date_fmt=$(date -d "$raw_date" +%Y-%m-%d 2>/dev/null)
+  else
+    date_fmt=""
+  fi
+
   xmllint --html --xpath "//div[contains(@class,'group')]/div[@class='hidden md:flex flex-col space-y-[6px]']/div[@class='grid grid-cols-12 items-center']" "$file" 2>/dev/null \
   | xmllint --html --xpath "//a/text() | //p[@class='col-span-2 text-neutral-500 text-[13px]']/text()" - 2>/dev/null \
   | awk 'ORS=NR%3?",":"\n"' \
-  | awk -v fname="$(basename "$file" -tags.html)" '{print fname","$1","$2","$3}' >> models/tmp.csv
+  | sed 's/,$//' \
+  | awk -v fname="$(basename "$file" -tags.html)" 'BEGIN{FS=",";OFS=","} {print fname,$1,$2,$3}' > models/tmp1.csv
+
+  tags=$(xmllint --html --xpath "//div[contains(@class,'flex flex-wrap space-x-2')]" "$file" 2>/dev/null \
+    | grep -oP '<span[^>]*>[^<]*</span>' \
+    | sed -E 's/<[^>]+>//g' \
+    | paste -sd'|' -)
+
+  awk -v tags="$tags" -v date="$date_fmt" -F, 'BEGIN{OFS=","} {print $0, tags, date}' models/tmp1.csv >> models/tmp.csv
 done
 
-echo "model,name,size,context" > docs/models.csv
+echo "model,name,size,context,tags,date" > docs/models.csv
 awk -F, '{
   size=$3;
   if(size ~ /TB/) val=substr(size,1,length(size)-2)*1024*1024*1024*1024;
@@ -31,5 +46,5 @@ awk -F, '{
   else ctxval=ctx;
   $4=ctxval;
 
-  OFS=","; printf "%s,%s,%d,%d\n", $1, $2, $3, $4
-}' models/tmp.csv >> docs/models.csv
+  OFS=","; printf "%s,%s,%d,%d,%s,%s\n", $1, $2, $3, $4, $5, $6
+}' models/tmp.csv | sort -u >> docs/models.csv
